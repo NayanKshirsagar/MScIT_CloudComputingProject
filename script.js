b3eb8a3738a315aea0cdbebd41ae8fa3b0c35659
NayanKshirsagar <nayankshirsagar0101@gmail.com>
2024-09-05 07:47:50 +0530

    3rd commit

--- script.js
+++ script.js
@@ -1,9 +1,40 @@
-// Function to trigger Learn More action
-function learnMore() {
-  alert("Learn more about the destination and exciting packages.");
+let generateImageForm = 
+    document.getElementById('generate-image-form');
+let formInput = 
+    document.getElementById('input-value');
+let imageContainerText = 
+    document.getElementById('imageContainerText');
+let imageGenerated = 
+    document.getElementById('generated-image');
+let imageContainer = 
+    document.getElementById('images-visible');
+
+async function fetchImages(category) {
+    try {
+        let response = 
+        await fetch(`use a API`);
+        if (!response.ok) {
+            throw new Error('Unable to fetch the data');
+        }
+        imageContainerText.innerText = 
+        "Below is your generated Image:";
+        imageContainer.style.display = "block";
+        imageGenerated.src = response.url;
+        console.log(response.url);
+    }
+    catch (error) {
+        console.log(error);
+    }
 }
 
-// Function to trigger Book Now action
-function bookNow() {
-  alert("Redirecting to booking page.");
-}
+generateImageForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let enteredText = formInput.value;
+    if (enteredText !== "") {
+        fetchImages(enteredText);
+    }
+    else {
+        imageContainerText.innerText = 
+            "Input field can not be empty!";
+    }
+})
